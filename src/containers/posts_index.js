27c53d2609cda1a8cdf7769d298c205c3af7ae05
liketysplit <rjboles2@gmail.com>
2018-10-15 22:14:56 -0400

    2.0-2.5: Getting posts to show

---
+++ src/containers/posts_index.js
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { fetchPosts } from "../actions/index";
+import _ from "lodash";
+
+class PostsIndex extends Component {
+	componentDidMount() {
+		this.props.fetchPosts();
+	}
+
+	renderPosts() {
+		return _.map(this.props.posts, post => {
+			return (
+				<li className="list-group-item" key={post._id}>
+					{post.title}
+				</li>
+			);
+		});
+	}
+
+	render() {
+		// console.log(this.props.posts);
+		return (
+			<div className="container">
+				<h2>Posts</h2>
+				<ul className="list-group">{this.renderPosts()}</ul>
+			</div>
+		);
+	}
+}
+
+function mapStateToProps(state) {
+	return { posts: state.posts };
+}
+
+export default connect(
+	mapStateToProps,
+	{ fetchPosts }
+)(PostsIndex);
--- src/index.js
+++ src/index.js
@@ -2,16 +2,29 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
-import App from "./components/app";
-import reducers from "./reducers";
+import { BrowserRouter, Route } from "react-router-dom";
+import PostsIndex from "./containers/posts_index";
+import ReduxPromise from "redux-promise";
+import { composeWithDevTools } from "redux-devtools-extension";
+// import App from "./components/app";
+import rootReducer from "./reducers";
 import "bootstrap/dist/css/bootstrap.min.css"; // import Bootstrap CSS library
 import "./index.css";
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
-
+// const createStoreWithMiddleware = applyMiddleware()(createStore);
+const store = createStore(
+	rootReducer,
+	{},
+	composeWithDevTools(applyMiddleware(ReduxPromise))
+);
 ReactDOM.render(
-	<Provider store={createStoreWithMiddleware(reducers)}>
-		<App />
+	<Provider store={store}>
+		<BrowserRouter>
+			<div>
+				<Route path="/api/posts" component={PostsIndex} />
+				<Route exact path="/" component={PostsIndex} />
+			</div>
+		</BrowserRouter>
 	</Provider>,
 	document.getElementById("root")
 );
