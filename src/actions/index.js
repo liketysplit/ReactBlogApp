94b59b476d5b9b80704100bc43dde21efda99b55
liketysplit <rjboles2@gmail.com>
2018-10-22 10:27:39 -0400

    Adding the ability to delete a post

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,6 +4,7 @@ import _ from "lodash";
 export const FETCH_POSTS = "fetch_posts";
 export const CREATE_POST = "create_post";
 export const FETCH_POST = "fetch_post";
+export const DELETE_POST = "fetch_post";
 
 const ROOT_URL = "https://blog-server-rick-boles.herokuapp.com/api";
 
@@ -36,5 +37,16 @@ export function fetchPost(id) {
 	return {
 		type: FETCH_POST,
 		payload: request
+	};
+}
+
+export function deletePost(id, callback) {
+	const request = axios
+		.delete(`${ROOT_URL}/posts/${id}`)
+		.then(() => callback());
+
+	return {
+		type: DELETE_POST,
+		payload: id
 	};
 }
--- src/containers/posts_show.js
+++ src/containers/posts_show.js
@@ -1,12 +1,19 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
-import { fetchPost } from "../actions";
+import { fetchPost, deletePost } from "../actions";
 
 class PostsShow extends Component {
 	componentDidMount() {
 		const { id } = this.props.match.params;
 		this.props.fetchPost(id);
+	}
+
+	onDeleteClick() {
+		const { id } = this.props.match.params;
+		this.props.deletePost(id, () => {
+			this.props.history.push("/");
+		});
 	}
 
 	render() {
@@ -25,6 +32,13 @@ class PostsShow extends Component {
 				<Link to="/" className="btn btn-primary">
 					Back to Index
 				</Link>
+
+				<button
+					className="btn btn-danger float-right"
+					onClick={this.onDeleteClick.bind(this)}
+				>
+					Delete Post
+				</button>
 
 				<div className="container">
 					<div className="row">
@@ -73,5 +87,5 @@ function mapStateToProps({ posts }, ownProps) {
 
 export default connect(
 	mapStateToProps,
-	{ fetchPost }
+	{ fetchPost, deletePost }
 )(PostsShow);
